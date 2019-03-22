dc6948939888defda290b858acf65894430c4794
Lukas Vukovich <lukas.vukovich@gmail.com>
2019-03-22 14:37:31 -0400

    Argument is not taking for const array (arr)

--- app.js
+++ app.js
@@ -11,3 +11,35 @@ const buttonStart = document.getElementsByClassName("btn__reset")[0];
 buttonStart.addEventListener("click", () => {
   overlay.style.display = "none";
 });
+
+// Create a phrases array that contains at least 5 different phrases as strings.
+
+const phrases = [
+  "Hotdogs are sandwiches",
+  "Poptarts are ravioli",
+  "No pain no gain",
+  "Supercalifragilisticexpialidocious",
+  "My name is Jeff"
+];
+
+// Create a getRandomPhraseAsArray function
+// The phrases array created in the previous step will be passed to this function
+// Step 1: Get a random phrase from the phrases array
+// Step 2: Split the random phrase into an array of separate strings
+
+function getRandomPhraseAsArray(arr) {
+  const randomPhrase = arr[Math.floor(Math.random() * arr.length)];
+  const split = randomPhrase.split(" "); // Returns ["Hotdogs", "are", "sandwiches"]
+  return split;
+}
+
+// Set the game display
+
+function addPhraseToDisplay(arr) {
+  const array = getRandomPhraseAsArray(arr);
+  const ul = document.querySelector("#phrase ul");
+  for (let i = 0; i <= array.length; i++) {
+    let add = array[i];
+    ul.appendChild(add);
+  }
+}
